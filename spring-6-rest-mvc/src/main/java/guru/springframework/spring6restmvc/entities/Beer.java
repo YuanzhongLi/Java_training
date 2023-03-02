1ed4f03b3d2dca499ccab78bdf28c2d01db89fa8
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-02 20:42:02 +0900

    add size validation annotation to bntity

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -28,6 +29,8 @@ public class Beer {
 
     @NotNull
     @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
 
     @NotNull
@@ -35,6 +38,7 @@ public class Beer {
 
     @NotNull
     @NotBlank
+    @Size(max = 255)
     private String upc;
     private Integer quantityOnHand;
 
