4b3aaad54030aee366b4161dd672b59414e7bd2e
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-05 13:25:56 +0900

    add one to one cascade

--- spring-6-rest-mvc/README.md
+++ spring-6-rest-mvc/README.md
@@ -1,5 +1,16 @@
 
+### データベース
+
 #### JPA データベースアクセス
 - [参考doc1](http://terasolunaorg.github.io/guideline/current/ja/ArchitectureInDetail/DataAccessDetail/DataAccessJpa.html)  
 O/R Mapping
 ![](./README_resources/JPA_OR_mapping_lifecyle.png)
+
+#### トランザクション分離レベル
+- [参考doc1](https://qiita.com/song_ss/items/38e514b05e9dabae3bdb)
+
+#### ロック
+- [楽観ロック/悲観ロック【optimistic locking/pessimistic locking】](https://e-words.jp/w/%E6%A5%BD%E8%A6%B3%E3%83%AD%E3%83%83%E3%82%AF-%E6%82%B2%E8%A6%B3%E3%83%AD%E3%83%83%E3%82%AF.html#:~:text=%E6%A5%BD%E8%A6%B3%E3%83%AD%E3%83%83%E3%82%AF%2F%E6%82%B2%E8%A6%B3%E3%83%AD%E3%83%83%E3%82%AF%E3%80%90optimistic,%E3%81%99%E3%82%8B%E3%81%8B%E3%82%92%E8%A1%A8%E3%81%99%E5%88%86%E9%A1%9E%E3%80%82)
+
+#### 暗黙的なコミット
+- [参考doc1](https://gihyo.jp/dev/serial/01/mysql-road-construction-news/0134)
--- spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -2,11 +2,11 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.BeerOrderShipment;
 import guru.springframework.spring6restmvc.entities.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,6 +38,9 @@ class BeerOrderRepositoryTest {
         BeerOrder beerOrder = BeerOrder.builder()
                 .customerRef("Test order")
                 .customer(testCustomer)
+                .beerOrderShipment(BeerOrderShipment.builder()
+                        .trackingNumber("1235r")
+                        .build())
                 .build();
 
         BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
