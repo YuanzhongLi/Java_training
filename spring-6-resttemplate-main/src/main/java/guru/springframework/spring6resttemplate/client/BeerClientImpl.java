47e75bda3fa54ae9073ede3d6cb8ca99afbf1a3a
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-06 11:46:42 +0900

    add base url config

--- spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,7 +1,7 @@
 package guru.springframework.spring6resttemplate.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -9,36 +9,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
-    private final RestTemplateBuilder restTemplateBuilder;
 
-    private static final String BASE_URL = "http://localhost:8080";
+    private final RestTemplateBuilder restTemplateBuilder;
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+        ResponseEntity<BeerDTOPageImpl> response =
+                restTemplate.getForEntity(GET_BEER_PATH , BeerDTOPageImpl.class);
 
-        ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
-
-        ResponseEntity<JsonNode> jsonResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                .elements().forEachRemaining(node -> {
-                    System.out.println(node.get("beerName").asText());
-                });
-
-        System.out.println(stringResponse.getBody());
-
-        return null;
+        return response.getBody();
     }
 }
