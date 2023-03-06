e3dbdae14e184a7a3bd32073b94880652c7c568a
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-06 12:49:19 +0900

    add rest put beer

--- spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 /**
@@ -24,13 +25,21 @@ public class BeerClientImpl implements BeerClient {
     private static final String GET_BEER_PATH = "/api/v1/beer";
     private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
 
+
+    @Override
+    public BeerDTO updateBeer(BeerDTO beerDto) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.put(GET_BEER_BY_ID_PATH, beerDto, beerDto.getId());
+        return getBeerById(beerDto.getId());
+    }
+
     @Override
     public BeerDTO createBeer(BeerDTO newDto) {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDto, BeerDTO.class);
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH, newDto);
 
-        return null;
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
     }
 
     @Override
