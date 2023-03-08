2bb1362ac5b787d91622fb91c3e1cbcf8c29b9d3
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-08 23:08:14 +0900

    add Beer Repository

---
+++ spring-6-reactive/src/main/java/guru/springframework/spring6reactive/repositories/BeerRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6reactive.repositories;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface BeerRepository extends ReactiveCrudRepository {
+}
