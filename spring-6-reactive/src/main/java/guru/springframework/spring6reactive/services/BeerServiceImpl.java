8852220e54b4617fe78f9b78a231810bf52d4cd2
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-09 11:08:06 +0900

    add get by id

--- spring-6-reactive/src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
+++ spring-6-reactive/src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
@@ -6,12 +6,18 @@ import guru.springframework.spring6reactive.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
+
+    @Override
+    public Mono<BeerDTO> getBeerById(Integer beerId) {
+        return beerRepository.findById(beerId).map(beerMapper::beerToBeerDto);
+    }
 
     @Override
     public Flux<BeerDTO> listBeers() {
