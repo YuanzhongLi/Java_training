866025de58c6345e871077095d4a22fae3de8380
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-10 22:16:41 +0900

    add test and test data

---
+++ spring-6-reactive-mongo/src/main/java/guru/springframework/reactivemongo/services/CustomerService.java
@@ -0,0 +1,9 @@
+package guru.springframework.reactivemongo.services;
+
+import guru.springframework.reactivemongo.model.CustomerDTO;
+import reactor.core.publisher.Mono;
+
+public interface CustomerService {
+    Mono<CustomerDTO> saveCustomer(CustomerDTO customerDTO);
+    Mono<CustomerDTO> getById(String customerId);
+}
