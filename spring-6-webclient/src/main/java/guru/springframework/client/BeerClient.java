3d441ecbb6737ba96ecd60119ec7cb8e1e78970b
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-11 12:16:52 +0900

    add url config

---
+++ spring-6-webclient/src/main/java/guru/springframework/client/BeerClient.java
@@ -0,0 +1,18 @@
+package guru.springframework.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.model.BeerDTO;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+import java.util.concurrent.Flow;
+
+public interface BeerClient {
+    Flux<String> listBeer();
+
+    Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
+}
