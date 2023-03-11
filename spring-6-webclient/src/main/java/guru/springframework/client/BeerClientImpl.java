66ba2b1c3da604a53eea9861179c6b7d15a08259
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-03-11 13:08:48 +0900

    add remain test

--- spring-6-webclient/src/main/java/guru/springframework/client/BeerClientImpl.java
+++ spring-6-webclient/src/main/java/guru/springframework/client/BeerClientImpl.java
@@ -22,6 +22,25 @@ public class BeerClientImpl implements BeerClient {
     }
 
     @Override
+    public Mono<BeerDTO> patchBeer(BeerDTO beerDTO) {
+        return webClient.patch()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID).build(beerDTO.getId()))
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(voidResponseEntity -> getBeerById(beerDTO.getId()));
+    }
+
+    @Override
+    public Mono<Void> deleteBeer(BeerDTO dto) {
+        return webClient.delete()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID).build(dto.getId()))
+                .retrieve()
+                .toBodilessEntity()
+                .then();
+    }
+
+    @Override
     public Mono<BeerDTO> updateBeer(BeerDTO beerDTO) {
         return webClient.put()
                 .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID).build(beerDTO.getId()))
